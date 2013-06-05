3fe11ef3ebe63b9dd2c11d1fab0fc0b047c24d13
Yihang Yang <372395698@qq.com>
2013-06-05 20:08:34 +0800

    unfinished

--- utils/comics.py
+++ utils/comics.py
@@ -20,7 +20,6 @@ class Volume(Base):
     def __getNewId(self):
         return self.getDao().redis_comics.incr("total_volume_id")
 
-    @property
     def getId(self):
         return self._id
 
@@ -60,6 +59,7 @@ class Volume(Base):
 class Comics(Base):
     """
     meta(json): include meta_args
+    volume_list(list): include volume list
     """
     meta_args = ["create_time", "update_time", "status", "title",
         "author", "cover", "uploader", "uploader_id", "introduction"]
@@ -73,7 +73,6 @@ class Comics(Base):
     def __getNewId(self):
         return self.getDao().redis_comics.incr("total_comics_id")
 
-    @property
     def getId(self):
         return self._id
 
@@ -91,6 +90,24 @@ class Comics(Base):
         assert set(value.keys()) >= set(meta_args)
         self._meta = value
         self.getDao().composite_comics.jset("comics:%d" % (self._id, ), self._meta)
+
+    @property
+    def volume_list(self):
+        volume_ids = self.getDao().composite_comics.lrange("comics:%d:volume_list" % (self._id, ), 0, -1)
+        return [Volume(volume_id=volume_id) for volume_id in volume_ids]
+
+    def addVolume(self, volumes):
+        volume_ids = map(lambda tmp: tmp.getId(), volumes)
+        self.getDao().composite_comics.rpush("comics:%d:volume_list" % (self._id, ), volume_ids)
+
+    def deleteVolume(self, delete_ids):
+        volume_ids = self.getDao().composite_comics.lrange("comics:%d:volume_list" % (self._id, ), 0, -1)
+        rest_set = set(volume_ids) - set(delete_ids)
+        #TODO
+        #improve the performance
+        rest_ids = [volume_id in rest_set for volume_id in volume_ids]
+        self.getDao().composite_comics.delete("comics:%d:volume_list" % (self._id, ))
+        self.getDao().composite_comics.rpush("comics:%d:volume_list" % (self._id, ), rest_ids)
 
     def save(self, meta):
         meta["create_time"] = int(time.time())
